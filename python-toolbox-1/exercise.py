3e7b207d6d21bc3a8fe21321aca0328c74c75a70
Zac Johnson <52793864+johnsonzr@users.noreply.github.com>
2023-06-07 15:34:12 -0400

    Update exercise.py

--- python-toolbox-1/exercise.py
+++ python-toolbox-1/exercise.py
@@ -1,6 +1,19 @@
-# Chapter 1: 
+# Chapter 2: 
+# Exercise 2:
+# Nested Functions 1
 
-# Exercise: [name of exercise]
+# Define three_shouts
+def three_shouts(word1, word2, word3):
+    """Returns a tuple of strings
+    concatenated with '!!!'."""
 
-# Exercise: [name of exercise]
+    # Define inner
+    def inner(word):
+        """Returns a string concatenated with '!!!'."""
+        return word + '!!!'
 
+    # Return a tuple of strings
+    return (inner(word1), inner(word2), inner(word3))
+
+# Call three_shouts() and print
+print(three_shouts('a', 'b', 'c'))
