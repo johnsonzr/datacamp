af0a47b38019fa31d6596bb681c9b5a680e2e81d
Zac Johnson <52793864+johnsonzr@users.noreply.github.com>
2023-07-31 19:04:25 +0000

    PythonToolbox2_Chapter1

---
+++ python-toolbox-2/slides/exercises_chapter1/using-enumerate.py
@@ -0,0 +1,20 @@
+# Create a list of strings: mutants
+mutants = ['charles xavier', 
+            'bobby drake', 
+            'kurt wagner', 
+            'max eisenhardt', 
+            'kitty pryde']
+
+# Create a list of tuples: mutant_list
+mutant_list = ____
+
+# Print the list of tuples
+print(mutant_list)
+
+# Unpack and print the tuple pairs
+for ____ in ____:
+    print(index1, value1)
+
+# Change the start index
+for ____ in ____:
+    print(index2, value2)
