4b28351f9d742e8d50716ea07a881e061369fd09
Zac Johnson <52793864+johnsonzr@users.noreply.github.com>
2023-10-12 15:59:41 +0000

    Chapter 1 exercises for data structures and algorithms

---
+++ data_structures_and_algorithms_in_python/exercises_chapter1/practicing_with_big_o_notation.py
@@ -0,0 +1,9 @@
+colors = ['green', 'yellow', 'blue', 'pink']
+
+def linear(colors):
+  # Iterate the elements of the list
+  for x in colors:
+    # Print the current element of the list
+    print(x)	
+
+linear(colors)
