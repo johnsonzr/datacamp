87c903311b38cdff5d98cdc87636a7c79188ef35
Zac Johnson <52793864+johnsonzr@users.noreply.github.com>
2024-03-14 18:45:42 +0000

    seaborn data visualization chapter 2 exercises

---
+++ intermediate_data_visualization_with_seaborn/exercises_chapter2/comparing_styles.py
@@ -0,0 +1,9 @@
+sns.displot(df['fmr_2'])
+sns.set_style('dark')
+plt.show()
+plt.clf()
+
+sns.displot(df['fmr_2'])
+sns.set_style('whitegrid')
+plt.show()
+plt.clf()
---
+++ intermediate_data_visualization_with_seaborn/exercises_chapter2/matplotlib_color_codes.py
@@ -0,0 +1,6 @@
+# Set style, enable color code, and create a magenta displot
+sns.set(color_codes=True)
+sns.displot(df['fmr_3'], color='m')
+
+# Show the plot
+plt.show()
