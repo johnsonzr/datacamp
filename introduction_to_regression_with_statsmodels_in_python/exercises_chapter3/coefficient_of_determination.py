ff945a9e38a4e2d4e1098d3dfc1376c3ecfbb19a
Zac Johnson <52793864+johnsonzr@users.noreply.github.com>
2024-07-23 15:30:41 +0000

    chapter 3 exercises

---
+++ introduction_to_regression_with_statsmodels_in_python/exercises_chapter3/coefficient_of_determination.py
@@ -0,0 +1,11 @@
+# Print a summary of mdl_click_vs_impression_orig
+print(mdl_click_vs_impression_orig.summary())
+
+# Print a summary of mdl_click_vs_impression_trans
+print(mdl_click_vs_impression_trans.summary())
+
+# Print the coeff of determination for mdl_click_vs_impression_orig
+print(mdl_click_vs_impression_orig.rsquared)
+
+# Print the coeff of determination for mdl_click_vs_impression_trans
+print(mdl_click_vs_impression_trans.rsquared)
