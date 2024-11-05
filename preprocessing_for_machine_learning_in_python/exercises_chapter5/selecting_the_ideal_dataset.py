be66b4c5f3b654225b77636fd50be264639e5138
Zac Johnson <52793864+johnsonzr@users.noreply.github.com>
2024-11-05 16:12:43 +0000

    exercises chapter 5 for preprocessing for machine learning in python

---
+++ preprocessing_for_machine_learning_in_python/exercises_chapter5/selecting_the_ideal_dataset.py
@@ -0,0 +1,8 @@
+# Make a list of features to drop
+to_drop = ['city', 'country', 'date', 'desc', 'lat', 'length_of_time', 'long', 'minutes', 'recorded', 'seconds', 'state']
+
+# Drop those features
+ufo_dropped = ufo.drop(columns = to_drop)
+
+# Let's also filter some words out of the text vector we created
+filtered_words = words_to_filter(vocab, vec.vocabulary_, desc_tfidf, 4)
