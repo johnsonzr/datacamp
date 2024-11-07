fbe74d59377080c093e26a36e6e28876088dad03
Zac Johnson <52793864+johnsonzr@users.noreply.github.com>
2024-11-07 17:53:32 +0000

    chapter 1 and 2 exercises for intermediate sequal

---
+++ intermediate_sql/exercises_chapter2/using_and.sql
@@ -0,0 +1,17 @@
+-- Select the title and release_year for all German-language films released before 2000
+SELECT title, release_year
+FROM films
+WHERE release_year < 2000 
+    AND language = 'German';
+
+    -- Update the query to see all German-language films released after 2000
+SELECT title, release_year
+FROM films
+WHERE release_year > 2000
+	AND language = 'German';
+
+    -- Select all records for German-language films released after 2000 and before 2010
+SELECT *
+FROM films
+WHERE release_year > 2000 AND release_year < 2010
+	AND language = 'German';
---
+++ intermediate_sql/exercises_chapter2/using_where_with_text.sql
@@ -0,0 +1,4 @@
+-- Count the Spanish-language films
+SELECT COUNT(language) as count_spanish
+FROM films
+WHERE language = 'Spanish'
